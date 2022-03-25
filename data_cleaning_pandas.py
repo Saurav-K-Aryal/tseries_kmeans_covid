e1afcbe0b145bf5a4106c3b1ca57e0c37b2e0c18
Saurav-K-Aryal <saurav.aryal@bison.howard.edu>
2022-03-25 14:01:48 -0400

    updating data_cleaning_pandas.py for means of numeric and mode of obj columns

--- data_cleaning_pandas.py
+++ data_cleaning_pandas.py
@@ -23,12 +23,15 @@ print('Using csv at', sys.argv[1])
 
 csv_file_path = sys.argv[1]
 # for debug only
-N_ROWS = 10000000
+# N_ROWS = 10000000
+
 # check if read_csv fails..
-cols = [col for col in columns if dtypes[col] != 'object']
-cols.extend(['country_name', 'iso_3166_1_alpha_3'])
+# numeric_cols = [col for col in columns if dtypes[col] != 'object']
+# object_cols = [col for col in column if col not in numeric_cols]
+# numeric_cols.extend(['country_name', 'iso_3166_1_alpha_3'])
+# object_cols.extend(['country_name', 'iso_3166_1_alpha_3'])
 try:
-	df = pd.read_csv(csv_file_path, dtype=dtypes, usecols=cols)
+	df = pd.read_csv(csv_file_path, dtype=dtypes, usecols=numeric_cols)
 	print(df.columns)
 	# print(df[['smoking_prevalence','diabetes_prevalence','stay_at_home_requirements','international_travel_controls','testing_policy']].head())
 except:
@@ -38,7 +41,9 @@ except:
 
 print('Grouping the following df')
 print(df.head())
-group_df = df.groupby(['country_name', 'iso_3166_1_alpha_3']).mean(numeric_only=True)
 
-print('Wrote the following df to grouped.csv', group_df)
-group_df.to_csv('grouped.csv')
+# take mean on numeric columns and mode on object columns
+agg_dict = dict((k,'mean') if dtypes[k] != 'object' else (k, lambda x: x.mode()) for k in dtypes)
+del agg_dict['country_name']
+del agg_dict['iso_3166_1_alpha_3']
+print(agg_dic)
