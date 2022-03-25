ddb96c7e6d495f96380b7bdf2c472f76869beb31
Saurav-K-Aryal <saurav.aryal@bison.howard.edu>
2022-03-25 14:32:07 -0400

    changed float64 to float32 for dtypes to consume less memory at the cost of some precision, updated data_cleaning_pandas.py to perform mean on numeric and mode on object columns -> output a csv

--- data_cleaning_pandas.py
+++ data_cleaning_pandas.py
@@ -25,6 +25,10 @@ csv_file_path = sys.argv[1]
 # for debug only
 # N_ROWS = 10000000
 
+print('Reading', len(columns), '# of columns')
+
+from time import time
+st = time()
 # check if read_csv fails..
 try:
 	df = pd.read_csv(csv_file_path, dtype=dtypes, usecols=columns)
@@ -39,11 +43,14 @@ print('Grouping the following df')
 print(df.head())
 
 # take mean on numeric columns and mode on object columns
+# create dict like in example here
+# https://stackoverflow.com/questions/48225508/pandas-get-the-average-and-mode-group-by-specified-columns
 agg_dict = dict((k,'mean') if dtypes[k] != 'object' else (k, lambda x: x.mode()) for k in dtypes)
 del agg_dict['country_name']
 del agg_dict['iso_3166_1_alpha_3']
 print(agg_dict)
 
 # group_by
-grouped_df = df.group_by(['country_name', 'iso_3166_1_alpha_3']).agg(agg_dict)
-group_by.to_csv('grouped_mean_modes.csv')
+grouped_df = df.groupby(['country_name', 'iso_3166_1_alpha_3']).agg(agg_dict)
+grouped_df.to_csv('grouped_mean_modes.csv')
+print('Script takes', (time() - st) / 60, 'minutes')
