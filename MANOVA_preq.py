3407cf1a8522d9e427b9396f1c81d26f7eb01bc1
Saurav-K-Aryal <saurav.aryal@bison.howard.edu>
2022-03-30 20:50:39 -0400

    update manova prereq

--- MANOVA_preq.py
+++ MANOVA_preq.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 # from pinguoin import multivariate_normality
 import pandas as pd
 import pingouin as pg
@@ -7,7 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler()
 
-df = pd.read_csv('/home/anirudd/Documents/tseries_kmeans_covid/combined_dfs.csv')
+df = pd.read_csv('combined_dfs.csv')
 # print(df.dtypes)
 df.drop(columns=['Unnamed: 0'], inplace=True)
 
@@ -20,14 +21,16 @@ features = df[col_names]
 scaler = MinMaxScaler()
 
 df[col_names] = scaler.fit_transform(features.values)
+col_names.append('labels')
+df = df[col_names]
 
+#
+print(df.head())
+# print(df.dtypes)
 
-# print(df.head())
-print(df.dtypes)
+df = df.astype({'labels' : 'category'})
 
-df = df.astype({'labels' : object})
-
-print(df.dtypes)
+# print(df.dtypes)
 # print(df.columns)
 # print(len(df.columns))
 
@@ -37,7 +40,20 @@ print(df.dtypes)
 
 grouped = df.groupby("labels")
 for name, group in grouped:
-    print(name, pg.multivariate_normality(group, alpha=.05)) 
+    # if name or name == 0:
+        # group.fillna(0, inplace=True)
+    print('cluster #', name)
+    print('Num countries in cluster', group.shape[0])
+
+    # since each of these 20 (except that one 19 which we ignore) or
+    # higher we can claim it is normal
+    # central limit theorem
+
+    # but let's plot each to see
+    group.hist(layout=(5, 3), color='blue', figsize=(32,32), grid=False)
+    plt.title('Histogram plot all numeric var in cluster' + str(name))
+    plt.subplots_adjust(hspace=1.0)
+    plt.show()
 
 
 
