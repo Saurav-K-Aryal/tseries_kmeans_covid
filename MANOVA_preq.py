92f529cda48735556311016982b40c5b38556464
Aniruddha18-SPD <pokhrelaniruddha@gmail.com>
2022-04-02 14:08:29 -0400

    updated testing file

--- MANOVA_preq.py
+++ MANOVA_preq.py
@@ -4,6 +4,8 @@ import pandas as pd
 import pingouin as pg
 from pingouin import multivariate_normality
 from sklearn.preprocessing import MinMaxScaler
+from scipy.stats import bartlett
+from scipy.stats import levene
 
 scaler = MinMaxScaler()
 
@@ -23,23 +25,47 @@ df[col_names] = scaler.fit_transform(features.values)
 
 
 # print(df.head())
-print(df.dtypes)
+# print(df.dtypes)
 
 df = df.astype({'labels' : object})
 
-print(df.dtypes)
+# print(df.dtypes)
 # print(df.columns)
 # print(len(df.columns))
 
 # arr = ['t{0}'.format(x) for x in range(0, 600)]
 
+# grouped = df.groupby("labels")
+'''
+    # for name, group in grouped:
+    #     print(name, pg.multivariate_normality(group, alpha=.05)) 
 
+'''
 
 grouped = df.groupby("labels")
-for name, group in grouped:
-    print(name, pg.multivariate_normality(group, alpha=.05)) 
+
+lst = grouped.apply(pd.Series.tolist).tolist()
+kargs = []
 
 
+for outermost in lst:
+    for second_lyr in outermost:
+        # print(second_lyr)
+        second_lyr.pop(0)
+        second_lyr.pop(0)
+        del second_lyr[-2]
+        # del second_lyr[-1]
+        # print(second_lyr)
+        kargs.append(second_lyr)
 
+args = kargs
+# print(args)
+
+
+stat, p = bartlett(*args)
 
 # print(p)
+stat_1, l = levene(*args)
+
+print(stat, p)
+print(stat_1, l)
