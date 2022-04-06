09c5d381b78593b0a9f754c3eb8e2bcffad19425
Aniruddha18-SPD <pokhrelaniruddha@gmail.com>
2022-04-05 22:12:39 -0400

    poc hoc analysis added

--- MANOVA_analysis.py
+++ MANOVA_analysis.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import bartlett
 from scipy.stats import levene
 from statsmodels.multivariate.manova import MANOVA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
 scaler = MinMaxScaler()
 
 df = pd.read_csv('combined_dfs.csv')
@@ -28,7 +29,7 @@ df = df[col_names]
 
 #
 # print(df.head())
-print(df.dtypes)
+# print(df.dtypes)
 
 df = df.astype({'labels' : 'category'})
 
@@ -41,5 +42,33 @@ df = df.astype({'labels' : 'category'})
 
 
 
+
+
 maov = MANOVA.from_formula('average_temperature_celsius + comorbidity_mortality_rate + cumulative_persons_fully_vaccinated + cumulative_persons_vaccinated + cumulative_tested + diabetes_prevalence + elevation_m + gdp_per_capita_usd + gdp_usd + human_capital_index + latitude + population_density + smoking_prevalence ~ labels', data = df)
 print(maov.mv_test())
+
+
+X = df[['average_temperature_celsius','comorbidity_mortality_rate','cumulative_persons_fully_vaccinated','cumulative_persons_vaccinated','cumulative_tested','diabetes_prevalence','elevation_m','gdp_per_capita_usd','gdp_usd','human_capital_index','latitude','population_density','smoking_prevalence']].fillna(0)
+y = df["labels"].fillna(0)
+# print(X.shape, y.shape)
+# print(X.head)
+#print(y.head)
+post_hoc = lda().fit(X=X, y=y)
+
+# get Prior probabilities of groups:
+
+print("\n" + "The post hoc priors is: " + "\n")
+
+print(post_hoc.priors_)
+
+print("\n" + "The post hoc means is: " + "\n")
+
+print(post_hoc.means_)
+
+print("\n" + "The post hoc scaling is: " + "\n")
+
+print(post_hoc.scalings_)
+
+print("\n" + "The explained_variance_ratio is: " + "\n")
+
+print(post_hoc.explained_variance_ratio_)
